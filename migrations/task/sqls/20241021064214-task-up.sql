b53e8b30e429c117dd5dc158daa9d551b28ce695
DannyTan8x <dannytan8xtw@icloud.com>
2024-12-08 13:41:44 +0800

    upload task4

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -367,6 +367,41 @@ WHERE
 -- 5. 授課結束時間`end_at`設定為2024-11-25 16:00:00
 -- 6. 最大授課人數`max_participants` 設定為10
 -- 7. 授課連結設定`meeting_url`為 https://test-meeting.test.io
+insert into
+    "COURSE" (
+        user_id,
+        skill_id,
+        "name",
+        start_at,
+        end_at,
+        max_participants,
+        meeting_url
+    )
+values
+    (
+        (
+            select
+                id user_id
+            from
+                "USER"
+            where
+                "name" = '李燕容'
+        ),
+        (
+            select
+                id skill_id
+            from
+                "SKILL"
+            where
+                "name" = '重訓'
+        ),
+        '重訓基礎課',
+        '2024-11-25 14:00:00',
+        '2024-11-25 16:00:00',
+        10,
+        'https://test-meeting.test.io'
+    );
+
 -- ████████  █████   █    █████ 
 --   █ █   ██    █  █     █     
 --   █ █████ ███ ███      ████  
